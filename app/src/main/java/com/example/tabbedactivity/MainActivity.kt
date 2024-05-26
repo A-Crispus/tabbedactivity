ffc3621f9dc014b5abc008a87adb997456d6b3d6
augustrayziss <augustrayziss@gmail.com>
2024-05-26 23:34:37 +0300

    first commit

---
+++ app/src/main/java/com/example/tabbedactivity/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.tabbedactivity
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import com.example.tabbedactivity.ui.main.SectionsPagerAdapter
+import com.example.tabbedactivity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var textView : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = binding.viewPager
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = binding.fab
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+
+        }
+        var textView =findViewById<R.id.textView>
+        // time count down for 30 seconds,
+        // with 1 second as countDown interval
+        object : CountDownTimer(30000, 1000) {
+
+            // Callback function, fired on regular interval
+            override fun onTick(millisUntilFinished: Long) {
+                textView.setText("seconds remaining: " + millisUntilFinished / 1000)
+            }
+
+            // Callback function, fired
+            // when the time is up
+            override fun onFinish() {
+                textView.setText("done!")
+            }
+        }.start()
+
+
+    }
+}
